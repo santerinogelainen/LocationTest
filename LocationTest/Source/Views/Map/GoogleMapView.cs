3965a065edcab1c014e34d0145a84ca345a69a9b
Santeri Nogelainen <santeri.nogelainen@gmail.com>
2018-04-24 20:48:46 +0300

    move locationprovider to map

--- LocationTest/Source/Views/Map/GoogleMapView.cs
+++ LocationTest/Source/Views/Map/GoogleMapView.cs
@@ -21,7 +21,7 @@ namespace LocationTest.Views.Map
     /// <summary>
     /// Represents the googlemap part of the map. Cannot contain any custom openGL graphics, just the googlemap
     /// </summary>
-    public class GoogleMapView : SupportMapFragment, IOnMapReadyCallback, ValueAnimator.IAnimatorUpdateListener
+    public class GoogleMapView : SupportMapFragment
     {
 
 				/// <summary>
@@ -29,22 +29,11 @@ namespace LocationTest.Views.Map
 				/// </summary>
 				public event Action OnCreated;
 
-				private ValueAnimator Animation { get; set; }
-				private LatLng LocationBefore { get; set; }
-				private LatLng LocationAfter { get; set; }
-
-				// the google map
-				public GoogleMap GoogleMap { get; set; }
-
         // the options
         public GoogleMapOptions Options { get; set; }
 
         // the map that contains this GoogleMapView
         public Map Parent { get; set; }
-
-        // the location provider
-				// TODO: move LocationProvider to Map class
-        public LocationTest.Support.LocationProvider LocationProvider { get; set; }
 
         /// <summary>
         /// Create the GoogleMapView
@@ -54,19 +43,9 @@ namespace LocationTest.Views.Map
         public GoogleMapView(Map parent, GoogleMapOptions options) : base(){
             Parent = parent;
 
-						// init animation class for custom animations
-						Animation = new ValueAnimator();
-						Animation.AddUpdateListener(this);
-						Animation.SetInterpolator(new LinearInterpolator());
-
             // set the options and initialize GoogleMapView
             SetOptions(options);
             InitMap();
-
-            // start requesting location
-            LocationProvider = new LocationTest.Support.LocationProvider(Parent.Activity);
-            LocationProvider.OnLocationUpdate += OnLocationUpdate;
-						
         }
 
         /// <summary>
@@ -91,7 +70,7 @@ namespace LocationTest.Views.Map
             FragmentTransaction transaction = manager.BeginTransaction();
             transaction.Add(Parent.Id, this, "mapfragment");
             transaction.Commit();
-            GetMapAsync(this);
+            GetMapAsync(Parent);
 				}
 
         /// <summary>
@@ -106,45 +85,6 @@ namespace LocationTest.Views.Map
             view.LayoutParameters.Width = ViewGroup.LayoutParams.MatchParent;
 						
 						OnCreated?.Invoke();
-				}
-
-        /// <summary>
-        /// Called when the google map is ready
-        /// </summary>
-        /// <param name="map">The map</param>
-        public void OnMapReady(GoogleMap map)
-        {
-            GoogleMap = map;
-
-            // disable gestures, since the containing map (Parent) handles all gestures
-            GoogleMap.UiSettings.SetAllGesturesEnabled(false);
-				}
-
-				/// <summary>
-				/// Called when the location updates in the provider
-				/// </summary>
-				/// <param name="before"></param>
-				/// <param name="after"></param>
-				public void OnLocationUpdate(Location before, Location after)
-        {
-						if (before != null && after != null)
-						{
-								LocationBefore = LocationTest.Support.Convert.ToLatLng(before);
-								LocationAfter = LocationTest.Support.Convert.ToLatLng(after);
-								Animation.SetIntValues(0, 1000);
-								Animation.SetDuration(Settings.Location.UpdateInterval);
-								Animation.Start();
-						}
-        }
-
-				public void OnAnimationUpdate(ValueAnimator animation)
-				{
-						LatLng final = new LatLng(LocationBefore.Latitude + Animation.AnimatedFraction * (LocationAfter.Latitude - LocationBefore.Latitude), LocationBefore.Longitude + Animation.AnimatedFraction * (LocationAfter.Longitude - LocationBefore.Longitude));
-						//D.WL(final);
-
-						CameraPosition.Builder camera = new CameraPosition.Builder(GoogleMap.CameraPosition);
-						camera.Target(final);
-						GoogleMap.MoveCamera(CameraUpdateFactory.NewCameraPosition(camera.Build()));
 				}
 		}
 }
--- LocationTest/Source/Views/Map/Map.cs
+++ LocationTest/Source/Views/Map/Map.cs
@@ -8,6 +8,9 @@ using Android.Views;
 using LocationTest.Views.Support;
 using LocationTest.Support;
 using Android.Util;
+using Android.Locations;
+using Android.Animation;
+using Android.Views.Animations;
 
 namespace LocationTest.Views.Map
 {
@@ -16,9 +19,17 @@ namespace LocationTest.Views.Map
     /// 
     /// This is a wrapper class for GoogleMapView that actually contains the googlemap. This class contains touch events that manipulate the view.
     /// </summary>
-    public class Map : TouchableLayout
-    {
+    public class Map : TouchableLayout, ValueAnimator.IAnimatorUpdateListener, IOnMapReadyCallback
+		{
 
+				// animation values
+				private ValueAnimator Animator { get; set; }
+				private LatLng LocationBefore { get; set; }
+				private LatLng LocationAfter { get; set; }
+
+				/// <summary>
+				/// Called when the bearing changes on the map
+				/// </summary>
 				public event Action<float> OnRotate;
 
         // the parent activity
@@ -36,13 +47,23 @@ namespace LocationTest.Views.Map
 				// map compass
 				public Compass Compass { get; set; }
 
-        public Map(Context context) : base(context)
+				// the google map
+				public GoogleMap GoogleMap { get; set; }
+
+				// the location provider
+				public LocationTest.Support.LocationProvider LocationProvider { get; set; }
+
+				public Map(Context context) : base(context)
         {
             Activity = (FragmentActivity)context;
 
-            Post(OnViewCreated);
-        }
+						// init animation class for custom animations
+						Animator = new ValueAnimator();
+						Animator.AddUpdateListener(this);
+						Animator.SetInterpolator(new LinearInterpolator());
 
+						Post(OnViewCreated);
+        }
 
 				public Map(Context context, IAttributeSet attrs) : this(context) { }
 				public Map(Context context, IAttributeSet attrs, int defStyle) : this(context) { }
@@ -56,12 +77,37 @@ namespace LocationTest.Views.Map
 						MapView = new GoogleMapView(this, Settings.GoogleMapOptions);
 						// add compass after the mapview has been created, so that the compass is on top of the mapview
 						MapView.OnCreated += AddCompass;
+						MapView.OnCreated += StartLocationUpdates;
         }
+
+				private void StartLocationUpdates()
+				{
+						// start requesting location
+						LocationProvider = new LocationTest.Support.LocationProvider(Activity);
+						LocationProvider.OnLocationUpdate += OnLocationUpdate;
+				}
+
+				/// <summary>
+				/// Called when the location updates in the provider
+				/// </summary>
+				/// <param name="before">location before update</param>
+				/// <param name="after">location after update</param>
+				public void OnLocationUpdate(Location before, Location after)
+				{
+						if (before != null && after != null)
+						{
+								LocationBefore = LocationTest.Support.Convert.ToLatLng(before);
+								LocationAfter = LocationTest.Support.Convert.ToLatLng(after);
+								Animator.SetIntValues(0, 1000);
+								Animator.SetDuration(Settings.Location.UpdateInterval);
+								Animator.Start();
+						}
+				}
 
 				/// <summary>
 				/// Add a compass to this layout
 				/// </summary>
-				public void AddCompass()
+				private void AddCompass()
 				{
 						AddView(Compass = new Compass(this, Resource.Drawable.placeholder, Resource.Drawable.placeholder));
 				}
@@ -69,59 +115,100 @@ namespace LocationTest.Views.Map
         public override void OnTouchStart(MotionEvent e)
         {
 						// set the bearing that we start with
-            StartBearing = MapView.GoogleMap.CameraPosition.Bearing;
+            StartBearing = GoogleMap.CameraPosition.Bearing;
         }
 
-        public override void OnTouchMove(MotionEvent e)
-        {
-            CameraPosition.Builder camera = new CameraPosition.Builder(MapView.GoogleMap.CameraPosition);
+				public void OnAnimationUpdate(ValueAnimator animation)
+				{
+						LatLng final = new LatLng(LocationBefore.Latitude + Animator.AnimatedFraction * (LocationAfter.Latitude - LocationBefore.Latitude), LocationBefore.Longitude + Animator.AnimatedFraction * (LocationAfter.Longitude - LocationBefore.Longitude));
+
+						CameraPosition.Builder camera = new CameraPosition.Builder(GoogleMap.CameraPosition);
+						camera.Target(final);
+						GoogleMap.MoveCamera(CameraUpdateFactory.NewCameraPosition(camera.Build()));
+				}
+
+				/// <summary>
+				/// Called when the google map is ready
+				/// </summary>
+				/// <param name="map">The map</param>
+				public void OnMapReady(GoogleMap map)
+				{
+						GoogleMap = map;
+
+						// disable gestures, since the containing map (Parent) handles all gestures
+						GoogleMap.UiSettings.SetAllGesturesEnabled(false);
+				}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+				/// <summary>
+				/// Welcome to the depths of hell
+				/// </summary>
+				/// <param name="e"></param>
+				public override void OnTouchMove(MotionEvent e)
+				{
+						CameraPosition.Builder camera = new CameraPosition.Builder(GoogleMap.CameraPosition);
 						float Bearing;
 
-            // this is a bit complicated
+						// this is a bit complicated
 
-            // if the currect touch position is on the bottom half of the screen
-            // or just below the centerpoint
-            if (TouchCurrent.Y > CenterPoint.Y)
-            {
-                // If the start position was on the top half of the screen that means we crossed the center point
-                // reset the touch so that the controls dont bug out
-                if (TouchStart.Y <= CenterPoint.Y)
-                {
-                    ResetTouchStart(e);
-                    return;
-                }
-								
+						// if the currect touch position is on the bottom half of the screen
+						// or just below the centerpoint
+						if (TouchCurrent.Y > CenterPoint.Y)
+						{
+								// If the start position was on the top half of the screen that means we crossed the center point
+								// reset the touch so that the controls dont bug out
+								if (TouchStart.Y <= CenterPoint.Y)
+								{
+										ResetTouchStart(e);
+										return;
+								}
+
 								// otherwise we can move the camera most likely
-                float MinusBearing;
+								float MinusBearing;
 
 								// check if we are on the right half of the screen currently
 								// or on the right side of the center point
-                if (TouchCurrent.X > CenterPoint.X)
-                {
+								if (TouchCurrent.X > CenterPoint.X)
+								{
 										// again check if we went over the left and right side of the screen divider by looking at the touch starting position
 										// and reset since it bugs out otherwise
 										if (TouchStart.X <= CenterPoint.X)
-                    {
-                        ResetTouchStart(e);
+										{
+												ResetTouchStart(e);
 												return;
-                    }
+										}
 										// calculate the bearing that we subtract from the starting bearing
-                    MinusBearing = ((TouchDelta.X - TouchDelta.Y) / Settings.Gestures.BearingSpeed);
-                }
+										MinusBearing = ((TouchDelta.X - TouchDelta.Y) / Settings.Gestures.BearingSpeed);
+								}
 								// we are on the left half of the screen currently
 								// or on the left side of the center point
 								else
-                {
+								{
 										// again check if we went over the left and right side of the screen divider by looking at the touch starting position
 										// and reset since it bugs out otherwise
-                    if (TouchStart.X >= CenterPoint.X)
-                    {
-                        ResetTouchStart(e);
+										if (TouchStart.X >= CenterPoint.X)
+										{
+												ResetTouchStart(e);
 												return;
 										}
 										// calculate the bearing that we subtract from the starting bearing
 										MinusBearing = ((TouchDelta.X + TouchDelta.Y) / Settings.Gestures.BearingSpeed);
-                }
+								}
 
 								// set the camera bearing
 								Bearing = StartBearing - MinusBearing;
@@ -134,10 +221,10 @@ namespace LocationTest.Views.Map
 								// If the start position was on the top half of the screen that means we crossed the center point
 								// reset the touch so that the controls dont bug out
 								if (TouchStart.Y >= CenterPoint.Y)
-                {
-                    ResetTouchStart(e);
+								{
+										ResetTouchStart(e);
 										return;
-                }
+								}
 
 								// otherwise we can move the camera most likely
 								float PlusBearing;
@@ -145,17 +232,17 @@ namespace LocationTest.Views.Map
 								// check if we are on the right half of the screen currently
 								// or on the right side of the center point
 								if (TouchCurrent.X > CenterPoint.X)
-                {
+								{
 										// again check if we went over the left and right side of the screen divider by looking at the touch starting position
 										// and reset since it bugs out otherwise
 										if (TouchStart.X <= CenterPoint.X)
-                    {
-                        ResetTouchStart(e);
+										{
+												ResetTouchStart(e);
 												return;
-                    }
+										}
 										// calculate the bearing that we subtract from the starting bearing
 										PlusBearing = ((TouchDelta.X + TouchDelta.Y) / Settings.Gestures.BearingSpeed);
-                }
+								}
 								// we are on the left half of the screen currently
 								// or on the left side of the center point
 								else
@@ -163,13 +250,13 @@ namespace LocationTest.Views.Map
 										// again check if we went over the left and right side of the screen divider by looking at the touch starting position
 										// and reset since it bugs out otherwise
 										if (TouchStart.X >= CenterPoint.X)
-                    {
-                        ResetTouchStart(e);
+										{
+												ResetTouchStart(e);
 												return;
-                    }
+										}
 										// calculate the bearing that we subtract from the starting bearing
 										PlusBearing = ((TouchDelta.X - TouchDelta.Y) / Settings.Gestures.BearingSpeed);
-                }
+								}
 
 								// set the camera bearing
 								Bearing = StartBearing + PlusBearing;
@@ -179,9 +266,9 @@ namespace LocationTest.Views.Map
 						OnRotate?.Invoke(Bearing);
 
 						// finally set the camera of the google map
-            MapView.GoogleMap.MoveCamera(CameraUpdateFactory.NewCameraPosition(camera.Build()));
-        }
+						GoogleMap.MoveCamera(CameraUpdateFactory.NewCameraPosition(camera.Build()));
+				}
 
-        
-    }
+
+		}
 }
